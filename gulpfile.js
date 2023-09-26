2453801c398e019a1ecb1a3ed872daac8d5dfebc
Kristijan Bujak <cileb411@gmail.com>
2023-09-26 13:35:09 +0200

    izbrisan browserSync jer mi ne treba

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
 
 // Paths
 const paths = {
@@ -21,21 +20,12 @@ function style() {
     .pipe(autoprefixer())
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
 // Watch for changes
 function watch() {
-  browserSync.init({
-    server: {
-      baseDir: './'
-    }
-  });
-
   gulp.watch(paths.styles.src, style);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp.watch('./src/**/*.min.css').on('change', browserSync.reload); // Watch min.css files in src folder
 }
 
 exports.style = style;
