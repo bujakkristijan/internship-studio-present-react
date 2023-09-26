7fad4d200ba87273c665720785d4b5c9dfce0fc2
Kristijan Bujak <cileb411@gmail.com>
2023-09-26 09:36:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/header/HeaderComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderComponent/>
+    </div>
+  );
+}
+
+export default App;
