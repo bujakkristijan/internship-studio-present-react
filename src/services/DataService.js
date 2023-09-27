7c2fdf01b257163c733daa2503fd08e8f27c7246
Kristijan Bujak <cileb411@gmail.com>
2023-09-27 22:56:07 +0200

    vracena putanja na staru, ne radi za gh-pages citanje iz json file-a

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,7 +1,10 @@
 class DataService{
     async loadProductsFromJSONFile(){
         try{
-            const response = await fetch('internship-studio-present-react/products-data/products.json');
+            //lokalno
+             const response = await fetch('/products-data/products.json');
+            //kada deploy na gh-pages, mada ne radi!!
+            // const response = await fetch('internship-studio-present-react/products-data/products.json');
             const data = await response.json();
             return data;
         }catch(error){
