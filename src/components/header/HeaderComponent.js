c8988684eda95daa118e53abe7a83b05a50f79be
Kristijan Bujak <cileb411@gmail.com>
2023-09-27 14:43:20 +0200

    header uradjen, zapetljao sam se oko language text container-a language mobile container-a kada, kreirao je duplikat prilikom prvog rendera, kao da je okinuo jos jedan iz nekog razloga, ali radi okej sad

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import './HeaderComponent.min.css'; 
 
 const HeaderComponent = () => {
+
+    // const headerBoxTabContainer = document.getElementById("header-box-tab-container");
+    const hasInitialized = useRef(false);
+    useEffect(() => {
+        if (!hasInitialized.current) {
+            initializeTabsLanguageIcon();
+            hasInitialized.current = true;
+          }
+    }, []);
+    
+
+    function showMenu() {
+        
+        const headerBoxTabContainer = document.getElementById("header-box-tab-container");
+        headerBoxTabContainer.style.right = "0px";
+        headerBoxTabContainer.style.boxShadow = "0 0 0 10000px rgba(0, 0, 0, .50)";
+        // document.body.style.overflow = "hidden";
+      }
+    
+      function hideMenu(){
+       
+        const headerBoxTabContainer = document.getElementById("header-box-tab-container");
+        headerBoxTabContainer.style.right = "-300px";
+        headerBoxTabContainer.style.boxShadow = "0 0 0 0 rgba(0,0,0,0)";
+        // document.body.style.overflow = "hidden";
+      }
+    
+      function initializeTabsLanguageIcon(){
+        const headerBoxTabContainer = document.getElementById("header-box-tab-container");
+        const xIcon = document.querySelector(".fa.fa-times");
+      
+        // kreiram container gde cu dodavati child elemente za svaki jezik posebno
+        const languageMobileContainer = document.createElement("div");
+        languageMobileContainer.className = "language-mobile-container";
+      
+        // kreiram i dodajem element za srpski jezik kao child element na languageMobileContainer
+        const srpskiDiv = document.createElement("div");
+        srpskiDiv.className = "language-mobile-text";
+        srpskiDiv.textContent = "Srpski";
+        languageMobileContainer.appendChild(srpskiDiv);
+      
+        // kreiram i dodajem element za engleski jezik kao child element na languageMobileContainer
+        const englishDiv = document.createElement("div");
+        englishDiv.className = "language-mobile-text";
+        englishDiv.textContent = "English";
+        languageMobileContainer.appendChild(englishDiv);
+      
+        // kreiram i dodajem element za madjarski jezik kao child element na languageMobileContainer
+        const magyarDiv = document.createElement("div");
+        magyarDiv.className = "language-mobile-text";
+        magyarDiv.textContent = "Magyar";
+        languageMobileContainer.appendChild(magyarDiv);
+      
+        // dodajem languageMobileContainer pre xIcon, mada je xIcon position: absolute svakako;
+        // nema potrebe xIcon.nextSibling da bude, dovoljno je samo da doda pre xIcon, xIcon je position: absolute svakako da stoji gore u gornjem desnom cosku od header-box-2, koji je position: fixed, pa na osnovu njega se pozicionira tacno 
+        headerBoxTabContainer.insertBefore(languageMobileContainer, xIcon);
+      
+        // kreiram languageTextContainer koji ce biti prvi, odnosno pre container-a sa jezicima koji su dodati kao child elementi
+        const languageTextContainer = document.createElement("div");
+        languageTextContainer.className = "language-text-container";
+        languageTextContainer.textContent = "JEZIK";
+      
+        // stavljam languageTextContainer pre languageMobileContainer
+        headerBoxTabContainer.insertBefore(languageTextContainer, languageMobileContainer);
+      }
+
   return (
     <header id="header" class="header">
     <div id="header-logo-language" class="header-box-1">
@@ -15,7 +81,7 @@ const HeaderComponent = () => {
               </label>
         </div>
 
-        <i id="hamburger-menu-button" class="fa fa-bars" aria-hidden="true"></i>
+        <i id="hamburger-menu-button" class="fa fa-bars" aria-hidden="true" onClick={showMenu}></i>
        
         <div class="language-container">
             <label class="language-label">Srpski</label>
@@ -24,7 +90,7 @@ const HeaderComponent = () => {
         </div>
     </div>
     <div id="header-box-tab-container" class="header-box-2">
-        <i id="x-menu-button" class="fa fa-times" aria-hidden="true"></i>
+        <i id="x-menu-button" class="fa fa-times" aria-hidden="true" onClick={hideMenu}></i>
 
         <div id="header-tab-container" class="tab-container">
             
