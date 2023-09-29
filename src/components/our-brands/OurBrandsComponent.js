327d0e89d19f5eb738e3d46141f157e937079663
Kristijan Bujak <cileb411@gmail.com>
2023-09-29 11:18:35 +0200

    izmenjeno da se i brendovi citaju iz json fajla

--- src/components/our-brands/OurBrandsComponent.js
+++ src/components/our-brands/OurBrandsComponent.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './OurBrandsComponent.min.css';
+import DataService from '../../services/DataService';
 
 const OurBrandsComponent = () => {
+
+    const [brands, setBrands] = useState([]);
+
+    useEffect(() => {
+        getBrands();
+    }, [])
+
+
+    const getBrands = () =>{
+        DataService.loadBrandsFromJSONFile().then(response =>{
+            setBrands(response.brands);
+            console.log(response.brands);
+        }).catch((error) =>{
+            console.error("Error getting data", error);
+        })
+    }
+
   return (
     <div class="our-brands-container">
         <div class="our-brands-text-main-container">
@@ -11,7 +29,18 @@ const OurBrandsComponent = () => {
             <div class="our-brands-title-line"></div>
         </div>
         <div class="our-brands-image-main-container">
-            <div class="our-brands-image-container">
+
+            {
+                brands.map(
+                    brand =>
+                    <div class="our-brands-image-container" key={brand.id}>
+                        <img class="image-our-brand" src={brand.imagePath} alt="An example PNG image"/>
+                        <div class="our-brands-image-title">{brand.title}</div>
+                    </div>
+                )
+            }
+
+            {/* <div class="our-brands-image-container">
                 <img class="image-our-brand" src={require('../../images/dove-brand-logo.png')} alt="An example PNG image"/>
                 <div class="our-brands-image-title">Dove</div>
             </div>
@@ -34,7 +63,7 @@ const OurBrandsComponent = () => {
             <div class="our-brands-image-container">
                 <img class="image-our-brand" src={require("../../images/cif-brand-logo.png")} alt="An example PNG image"/>
                 <div class="our-brands-image-title">Cif</div>
-            </div>
+            </div> */}
         </div>
         <div class="button-all-brands-container">
             <button class="button-all-brands">Svi brendovi u nasem portfoliju</button>
--- src/services/DataService.js
+++ src/services/DataService.js
@@ -11,6 +11,19 @@ class DataService{
             console.log(error);
         }
     }
+
+    async loadBrandsFromJSONFile(){
+        try{
+            //lokalno
+            // const response = await fetch('/products-data/products.json');
+            //otkad sam deploy na gh-pages, mora ova putanja da bi radilo i lokalno i na gh-pages!!
+             const response = await fetch('/internship-studio-present-react/brands-data/brands.json');
+            const data = await response.json();
+            return data;
+        }catch(error){
+            console.log(error);
+        }
+    }
 }
 
 const dataServiceInstance = new DataService();
